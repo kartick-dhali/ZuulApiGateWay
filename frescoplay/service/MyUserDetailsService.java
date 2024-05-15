0f44b373efdee82148c7f4bc39ee02071d63540f
kartick-dhali <148072657+kartick-dhali@users.noreply.github.com>
2024-05-15 11:39:46 +0530

    Create MyUserDetailsService.java

---
+++ frescoplay/service/MyUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.fresco.apigateway.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import com.fresco.apigateway.model.User;
+import com.fresco.apigateway.model.UserPrincipal;
+import com.fresco.apigateway.repo.UserRepository;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new UserPrincipal(user);
+    }
+
+
+    public String getFullName(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return user.getFullname();
+    }
+}
